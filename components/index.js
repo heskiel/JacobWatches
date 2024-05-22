c47b91d4a40e5bb61d86d0ea93a5566665f4d55d
heskiel <78499813+heskiel@users.noreply.github.com>
2024-05-22 13:24:58 -0400

    Delete components directory

--- components/index.js
+++
@@ -1,7 +0,0 @@
-import { Form } from "react-router-dom";
-
-// Layout
-export { default as Header } from "./Layout/Header";
-export { default as Footer } from "./Layout/Footer";
-export { default as Master } from "./Layout/Master";
-// Home Page
