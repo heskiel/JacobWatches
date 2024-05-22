59ef1d365e08664f06315947019e9857bdbe8062
heskiel <78499813+heskiel@users.noreply.github.com>
2024-05-22 13:24:21 -0400

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,7 @@
+import { Form } from "react-router-dom";
+
+// Layout
+export { default as Header } from "./Layout/Header";
+export { default as Footer } from "./Layout/Footer";
+export { default as Master } from "./Layout/Master";
+// Home Page
